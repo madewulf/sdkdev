607b86de60f82f09d47d7d26737e905a1dd92751
Martin De Wulf <madewulf@multitasked.net>
2015-02-26 18:11:23 +0100

    Initial commit

--- sdk.dev/ViewController.m
+++ sdk.dev/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) UIWebView * webview;
 @end
 
 @implementation ViewController
@@ -17,6 +17,48 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.webview = [[UIWebView alloc] init];
+    self.webview.frame = self.view.frame;
+    [self.view addSubview:self.webview];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL: [NSURL URLWithString: @"http://vinchat.herokuapp.com"] cachePolicy: NSURLRequestUseProtocolCachePolicy timeoutInterval:10];
+    [self.webview loadRequest: request];
+    self.webview.scrollView.bounces = NO;
+
+}
+
+-( void) viewDidAppear:(BOOL)animated
+{
+    [self registerForKeyboardNotifications];
+}
+
+- (void)registerForKeyboardNotifications
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWasShown:)
+                                                 name:UIKeyboardDidShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillBeHidden:)
+                                                 name:UIKeyboardWillHideNotification object:nil];
+    
+}
+- (void)keyboardWasShown:(NSNotification*)aNotification
+{
+    NSDictionary* info = [aNotification userInfo];
+    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+
+   /* UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, kbSize.height, 0.0);
+    self.webview.scrollView.contentInset = contentInsets;
+    self.webview.scrollView.scrollIndicatorInsets = contentInsets;*/
+    CGSize screenSize = [UIScreen mainScreen].applicationFrame.size;
+    self.webview.frame = CGRectMake(0,0,screenSize.width,screenSize.height - kbSize.height);
+
+}
+
+// Called when the UIKeyboardWillHideNotification is sent
+- (void)keyboardWillBeHidden:(NSNotification*)aNotification
+{
+    self.webview.frame = self.view.frame;
 }
 
 - (void)didReceiveMemoryWarning {
